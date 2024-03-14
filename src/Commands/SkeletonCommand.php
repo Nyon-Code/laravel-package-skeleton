749fb13ce9115448268ea0892d1a6221d10fb330
SmithCZE <ondrejnyklicek@icloud.com>
2024-03-14 19:16:40 +0100

    Add new features to command Add translate boot and lang

--- src/Commands/SkeletonCommand.php
+++ src/Commands/SkeletonCommand.php
@@ -6,12 +6,46 @@ use Illuminate\Console\Command;
 
 class SkeletonCommand extends Command
 {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
     protected $signature = 'skeleton';
 
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
     protected $description = 'Command description';
 
+    /**
+     * Indicates whether the command should be shown in the Artisan command list.
+     *
+     * @var bool
+     */
+    protected $hidden = false;
+
+    /**
+     * The console command name aliases.
+     *
+     * @var array
+     */
+    protected $aliases = ['skltn'];
+
+    /**
+     *
+     *
+     * @var string
+     */
+    protected string $alias = 'skltn';
+
+    /**
+     * Execute the console command.
+     */
     public function handle(): void
     {
-        //
+        $this->info('Hello world');
     }
 }

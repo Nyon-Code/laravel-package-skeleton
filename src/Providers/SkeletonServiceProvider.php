5dbeb435ddabe1fd1cb84f050fa52acd67eeff17
ondrejnyklicek <ondrejnyklicek@icloud.com>
2024-03-18 20:15:13 +0100

    Update & delete bugs

--- src/Providers/SkeletonServiceProvider.php
+++ src/Providers/SkeletonServiceProvider.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace Vendor\LaralvePackageSkeleton\Providers;
+namespace Vendor\LaravelPackageSkeleton\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Vendor\LaralvePackageSkeleton\Commands\SkeletonCommand;
-use Vendor\LaralvePackageSkeleton\Facades\SkeletonFacade;
+use Illuminate\Foundation\Console\AboutCommand;
+use Vendor\LaravelPackageSkeleton\Commands\SkeletonCommand;
+use Vendor\LaravelPackageSkeleton\Facades\SkeletonFacade;
 
 class SkeletonServiceProvider extends ServiceProvider
 {
@@ -34,6 +35,17 @@ class SkeletonServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(paths: __DIR__.'/../../database/migrations');
         $this->loadRoutesFrom(path: __DIR__.'/../../routes/routes.php');
         $this->loadTranslationsFrom(path: __DIR__.'/../../lang', namespace: 'laravel-package-skeleton');
+        $this->loadJsonTranslationsFrom(path: __DIR__.'/../../lang');
+
+        /**
+         * Return package information in about
+         *
+         * ---
+         * php artisan about
+         */
+        AboutCommand::add( section: 'Package name', data: [
+            'Version' => fn() => ini_get( option: '1.0')
+        ]);
 
         /**
          * Determine if the application is running in the console.
@@ -42,35 +54,37 @@ class SkeletonServiceProvider extends ServiceProvider
             $this->commands([
                 SkeletonCommand::class,
             ]);
-
         }
 
         /**
          * Publish config file
          *
+         * ---
          * php artisan vendor:publish --tag=skeleton_config
          */
         $this->publishes([
-            __DIR__.'/../../database/migrations' => config_path('package_name.php')
-        ], 'skeleton_config');
+            __DIR__.'/../../database/migrations' => config_path(path: 'package_name.php')
+        ], groups: 'skeleton_config');
 
         /**
          * Publish database migration
          *
+         * ---
          * php artisan vendor:publish --tag=skeleton_migrations
          */
         $this->publishes([
-            __DIR__.'/../../database/migrations' => database_path('migrations')
-        ], 'skeleton_migrations');
+            __DIR__.'/../../database/migrations' => database_path(path: 'migrations')
+        ], groups: 'skeleton_migrations');
 
         /**
          * Publish view files
          *
+         * ---
          * php artisan vendor:publish --tag=skeleton_views
          */
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/package_name'),
-        ], 'skeleton_views');
+            __DIR__.'/../resources/views' => resource_path(path: 'views/vendor/package_name'),
+        ], groups: 'skeleton_views');
 
     }
 }
